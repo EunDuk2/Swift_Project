3ad75e6d37b8e4df9bc6a45cb84d9d6074257d09
eunduk2 <dmsqls6789@naver.com>
2024-11-26 19:00:23 +0900

    StudyTimeCheck init

---
+++ StudyTimeCheck/StudyTimeCheck/StudyTimeCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTimeCheckApp.swift
+//  StudyTimeCheck
+//
+//  Created by EUNSUNG on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyTimeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
