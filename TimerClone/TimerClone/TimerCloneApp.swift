4a8e5a9b8b5711de59815a3f7fc83b1d52feb24d
eunduk2 <dmsqls6789@naver.com>
2024-11-30 21:45:05 +0900

    TimerClone init

---
+++ TimerClone/TimerClone/TimerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerCloneApp.swift
+//  TimerClone
+//
+//  Created by EUNSUNG on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
