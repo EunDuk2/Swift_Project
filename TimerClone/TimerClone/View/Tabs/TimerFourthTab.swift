d8b597cec1596b2294b8705aef831ebb63203ece
eunduk2 <dmsqls6789@naver.com>
2024-12-04 17:24:17 +0900

    Navigation Bar 구현

--- TimerClone/TimerClone/View/Tabs/TimerFourthTab.swift
+++ TimerClone/TimerClone/View/Tabs/TimerFourthTab.swift
@@ -9,12 +9,31 @@ import SwiftUI
 
 struct TimerFourthTab: View {
     
+    @Environment(\.editMode) private var editMode
+    @State private var isEditing = false
+    
     var imageSystemName: String
     var tabName: String
     var tag: Int
     
     var body: some View {
-        Text("타이머")
+        NavigationView {
+            VStack {
+                Text("타이머")
+            }
+                .navigationTitle(Text("타이머"))
+                .navigationBarItems(leading: Button(action: {
+                    isEditing.toggle()
+                    editMode?.wrappedValue = isEditing ? .active : .inactive
+                    // 편집 버튼 액션
+                }, label: {
+                    isEditing ? Text("완료").bold() : Text("편집")
+                }), trailing: Button(action: {
+                    // + 버튼 액션
+                }, label: {
+                    Image(systemName: "plus")
+                }))
+        }
             .tabItem {
                 Image(systemName: imageSystemName)
                 Text(tabName)
